61a525a58b2720899f310d8347a3702e221b7952
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-08-28 19:37:50 +0200

    create run not finished, dindt know how to assign to dummie runner-user... we have to tackle that tomorrow

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -2,9 +2,13 @@ class RunsController < ApplicationController
 
   def new
     @run = Run.new
+    @dog = Dog.find(params[:dog_id])
   end
 
   def create
+    @dog = Dog.find(params[:dog_id])
+    @run = Booking.new(run_params)
+
   end
 
   def show
@@ -20,5 +24,11 @@ class RunsController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+
+  def run_params
+    params.require(:run).permit(:dog_id, :user_id, :status, :appointment, :duration, :price, :note, :park_id)
   end
 end
