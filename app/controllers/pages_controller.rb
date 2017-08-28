21e7094c62527d7455e1e46875ee08c62d629d19
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-08-28 17:41:25 +0200

    logged_in?-metho

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    before_action :logged_out?
+
+
   end
 
   def help
@@ -10,4 +13,9 @@ class PagesController < ApplicationController
   def become_runner
   end
 
+private
+
+  def logged_out?
+    current_user = nil
+  end
 end
