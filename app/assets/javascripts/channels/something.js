5ed0c9c279a8d9cd7bbc6f5f76c156741065ebba
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-08-29 19:18:11 +0200

    ksahjdjlkd

---
+++ app/assets/javascripts/channels/something.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+
+//   $('.formTwo').hide()
+
+//   var message = ('#messageToRunner').val();
+//   $('#button1').click(function(event){
+//     event.preventDefault();
+//     $('.formOne').hide();
+//     $('.formTwo').show();
+//     $('#messageToRunner2').val('message');
+//   });
+// });
+
