b18c5e1e322c3ccf939d7391a7943ffbefb371c3
Gerardo Bonilla <gerbonilla@gmail.com>
2017-09-04 22:09:30 +0200

    new new secret home page

--- app/assets/javascripts/tabs-navigation.js
+++ app/assets/javascripts/tabs-navigation.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
     $($(this).data('target')).removeClass('hidden');
   });
 
+  $(".dog-selection-menu-item").on("click", function(e){
+    console.log('hello');
+    $(".dog-selection-menu-item").removeClass('active-dog');
+    $(this).addClass('active-dog');
+    $(".dog-profile").addClass('hidden');
+    $($(this).data('target')).removeClass('hidden');
+  });
+
 });
