85b63a44186898fb757f6b680c7b90ad67272ac5
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-09-05 13:39:17 +0200

    new data to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,10 @@
 class Message < ApplicationRecord
+
+  belongs_to :run
+  belongs_to :user
+  validates :content, presence: true, allow_blank: false
+
+  def from?(some_user)
+    user == some_user
+  end
 end
