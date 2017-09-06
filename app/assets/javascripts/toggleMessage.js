51594bb0e549f7d8c6992b122f61df0576fd8ada
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-09-06 19:17:08 +0200

    scrolling down effect

--- app/assets/javascripts/toggleMessage.js
+++ app/assets/javascripts/toggleMessage.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     event.preventDefault();
     $('#toggleInbox').toggle();
     $('.toggleButton').toggle();
+    window.scrollBy(0,1000);
   });
   $('#closechat').click(function(){
     $('#toggleInbox').toggle();
